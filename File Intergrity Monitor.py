4fd1a3632a9cf9fc1a5231aafb3de0c5fe7d9cf6
FredXV <fab.blue.07@gmail.com>
2026-09-19 00:02:51 +0100

    Added check integrity and JSON so baseline survives when program is opened again

--- File Intergrity Monitor.py
+++ File Intergrity Monitor.py
@@ -1,5 +1,6 @@
 import hashlib
 import os
+import json
 
 #Storage for baseline fingerprints
 
@@ -8,6 +9,8 @@ baseline_data = {}
 def create_baseline():
 
     print ("\n--- Creating Baseline ---")
+
+    global baseline_data
 
     target_files = os.listdir("Monitored_files")
 
@@ -25,13 +28,43 @@ def create_baseline():
 
         baseline_data[filename] = file_hash
 
+    with open ("baseline.json", "w") as f:
+
+        json.dump(baseline_data, f)
+
+
     
 
 def check_integrity():
 
     print ("\n--- Running Integrity Check --- ")
 
-    pass
+    target_files = os.listdir("Monitored_files")
+
+    for filename in target_files:
+
+        file_path = os.path.join("Monitored_files", filename)
+        
+        with open (file_path, "rb") as f:
+        
+                file_bytes = f.read()
+        
+                current_hash = hashlib.sha256(file_bytes).hexdigest()
+                
+        print (f"Checking File: {filename}")
+        
+        if baseline_data[filename] == current_hash:
+
+            print ("File unchanged, Hashes are the same")
+
+        else:
+
+            print ("File Changed, Hashes are different")
+
+
+
+
+    
     
 
 def view_baseline():
@@ -48,7 +81,15 @@ def view_baseline():
         print (f"File: {filename} \nHash: {file_hash}\n")
     
     
+try:
 
+    with open ("baseline.json", "r") as f:
+
+        baseline_data = json.load(f)
+
+except FileNotFoundError:
+
+    pass
     
 
 #MENU
@@ -83,7 +124,7 @@ while True:
 
     elif user_choice == "4":
 
-        print ("\nExiting...")
+        print ("\nExiting...\n")
         break
 
     else:
